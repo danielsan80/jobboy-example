50911fefc6b65b8b8091a346b075d530f707d5db
Danilo Sanchi <danilo.sanchi@gmail.com>
2022-02-15 13:27:03 +0100

    add twig and a home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,12 +4,13 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
 
     /**
-     * @Route("/", name="home", method="GET")
+     * @Route("/", name="home", methods={"GET"})
      */
     public function home() {
         return $this->render('home.html.twig');
